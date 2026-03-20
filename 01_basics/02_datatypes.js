3e0a1dc7637f7605dc9fc06d0f8aa04957657b40
Deep-shikha-Yadav <yadavdeepshikha887@gmail.com>
2026-03-20 17:27:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)  we are using node.js not browser this will create  error 
+//variable name numbers=>2 to the power 53 ,bigint, string=>"" ,boolean=>true/false, null=>standlone value,undefined
+//symbol=>unique 
+console.log(typeof undefined)
+console.log(typeof null)//object
+                 
