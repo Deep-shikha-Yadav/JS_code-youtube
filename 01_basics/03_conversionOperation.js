5511960193bbddc149d622c07e10d0661d4c59f2
Deep-shikha-Yadav <yadavdeepshikha887@gmail.com>
2026-03-20 18:17:05 +0000

    conversion of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -7,7 +7,30 @@
 //conversion of boolean
 let isLoggedIn=1;
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
-console.log(typeof booleanIsLoggedIn) 
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn) 
 //NOTES: 1=>true,0=>false,""=>false, "any string"=>true
+
+
+//*****************************************OPERATIONS***************************************
+let value=3
+let negValue=-value
+// console.log(negValue)
+// console.log(2+2) 
+// console.log(2-2) 
+// console.log(2*2) 
+// console.log(2**3) 
+// console.log(2/2) 
+let str1="HELLO"
+let str2=" DEEPSHIKHA"
+let str3=str1+str2
+console.log(str3)//we can also add string
+
+//IMPORTANT
+console.log("1"+"2")  //12
+console.log("1"+2)  //12
+console.log(1+2+"2")  //32
+console.log(1+"2"+2)  //122
+console.log("1"+2+2)  //122
+
 
