4eab61f12f5ff135c198469245fc59923f7dbc31
Deep-shikha-Yadav <yadavdeepshikha887@gmail.com>
2026-03-20 17:54:56 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+// let score="33abc"
+// console.log( typeof score)
+// let valueInNumber=Number(score)
+// console.log( typeof valueInNumber)
+// console.log(valueInNumber)
+
+//conversion of boolean
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn) 
+//NOTES: 1=>true,0=>false,""=>false, "any string"=>true
+
