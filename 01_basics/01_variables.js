781a0c662d0e36b21ccb2c4f9d2de2831c9c986a
Deep-shikha-Yadav <yadavdeepshikha887@gmail.com>
2026-03-20 17:32:05 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId =2024106
+//const keyword cannot change these are fix  (NOT ALLOWED)
+let accountEmail="yadavdeep1123344@gamil.com"
+var accountPassword="125245637"
+accountCity ="UTTARPRADESH" 
+let accountStae;
+
+// prefer not to use var bcz of issue in block scope and functional scope
+
+//accountId =2
+accountEmail="yadavdeep332255@gmail.com"
+accountPassword="998877"
+accountCity="LUCKNOW"
+
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity)  ;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountStae]);
