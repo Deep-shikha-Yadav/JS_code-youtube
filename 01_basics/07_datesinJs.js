2372df8dca0b2ed1427bb3d64fa0b3d0345a87d5
Deep-shikha-Yadav <yadavdeepshikha887@gmail.com>
2026-03-21 10:05:04 +0000

    DATES

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,6 @@
+//DATES
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())  //month started from 0 in JS
+console.log(typeof myDate)
