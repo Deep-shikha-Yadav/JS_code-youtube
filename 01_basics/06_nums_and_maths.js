a9b77d9db51d6e578894258a625c3dc6db5522eb
Deep-shikha-Yadav <yadavdeepshikha887@gmail.com>
2026-03-21 09:06:57 +0000

    06_maths and nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,38 @@
+// const score=400
+// console.log(score)
+
+// const balance =new Number(100)
+// console.log(balance)
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(5))
+
+// const balance=123.897
+// console.log(balance.toPrecision(5))
+// console.log(balance.toPrecision(4))
+// console.log(balance.toPrecision(3))
+// console.log(balance.toPrecision(6))
+
+
+             //++++++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-9))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+//  console.log(Math.round(4.2))
+//   console.log(Math.round(4.6))
+
+
+//math.random=> THIS I SVERY IMPORTANT AND VALUE LIES BETWEEN 0 TO 1
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min) //adding 1 to avoid 0 case
+
+
+
+
+
