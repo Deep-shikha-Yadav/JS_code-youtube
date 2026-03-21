fb9c19a2cadb5d57dfd9253452ad79cb865ba3bc
Deep-shikha-Yadav <yadavdeepshikha887@gmail.com>
2026-03-21 08:32:08 +0000

    05_string.

---
+++ 01_basics/05_string.js
@@ -0,0 +1,18 @@
+const name= "DEEPSHIKHA"
+const repoCount=50
+//console.log(name+repoCount) this is not good method  we are using 
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`) //this is string 
+// interpulation and bettermethod for developer
+let gameName= new String('DEEPSHIKHA')
+// console.log(gameName[0])
+// console.log(gameName.toLowerCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('P'))
+// console.log(gameName.substring(0,5))
+//console.log(gameName.substring.trim())// for removing extra spaces in string 
+
+
+const url= "httpppp625jqp_github_dev"
+console.log(url.replace('_',':/'))
+//convert string to array on the basic of split
+console.log(url.split('_'))
