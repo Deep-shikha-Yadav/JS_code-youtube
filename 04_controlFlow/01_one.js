a78ad9166824c6ad20e81fa970fd700f8dfb1076
Deep-shikha-Yadav <yadavdeepshikha887@gmail.com>
2026-03-23 06:15:22 +0000

    01_one

---
+++ 04_controlFlow/01_one.js
@@ -0,0 +1,23 @@
+//IF
+
+// if(condition){  //this must be true to excute this condition
+  
+// }
+// const IsUserLoggedIn= true
+
+// if(IsUserLoggedIn){
+
+// }
+//<,>,<=,>=,==,!=,===
+//= this is assign operator
+//== we are checking that equal or not  (2=="2")=>equal in this case
+// === this also check the type of value (2==="2")=> npt equal bcz one is string and another one is number
+ 
+const score=200
+if(score>100){
+    const power="fly"
+    console.log(`user power ${power}`);
+}
+//var datatype  is work outside of localscope means it is work like global scope
+
+
