a1764c5b870cfff8df1c1b0b4fbaf6431e216f7c
Deep-shikha-Yadav <yadavdeepshikha887@gmail.com>
2026-03-23 06:27:29 +0000

    02_switches

---
+++ 04_controlFlow/02_switch.js
@@ -0,0 +1,26 @@
+// switch(key){
+//     case value:
+//         break;
+//         default:
+//             break;
+// }
+
+const month = 3
+switch(month){
+    case 1:
+        console.log("JANUARY")
+        break;
+        case 2:
+            console.log("FEBUARY")
+        break;
+        case 3:
+             console.log("MARCH")
+        break;  // if we not use break then all code will execute after condition match except default
+        case 4:
+             console.log("APRIL")
+        break;
+        default:
+             console.log("NO MONTH")
+            break;
+}
+
