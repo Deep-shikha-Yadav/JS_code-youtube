93bd1de7ece376567aa698669e2af2a8885da87d
Deep-shikha-Yadav <yadavdeepshikha887@gmail.com>
2026-03-23 05:36:46 +0000

    scope.js

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,19 @@
+function one(){
+    const username="DEEPSHIKHA"
+    function two(){
+        const website ="YOUTUBE"
+        //console.log(username);
+    }
+    two()
+
+}
+//one()
+if(true){                  //Always if condition will work
+const username="DEEPSHIKHA"
+if(username==="DEEPSHIKHA"){
+    const website ="YOUTUBE"
+    console.log(username + website)
+}
+ console.log(website);
+}
+console.log(username);
